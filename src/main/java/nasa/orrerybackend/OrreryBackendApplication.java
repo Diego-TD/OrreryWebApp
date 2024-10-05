29688423d7bd998300a0027a6cf439da2b2bfaee
DiegoTD <diegotorresdurante@gmail.com>
2024-10-05 08:55:54 -0700

    initial commit

---
+++ src/main/java/nasa/orrerybackend/OrreryBackendApplication.java
@@ -0,0 +1,13 @@
+package nasa.orrerybackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrreryBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrreryBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/nasa/orrerybackend/OrreryBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package nasa.orrerybackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrreryBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
